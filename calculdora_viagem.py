fb6b9117241c6fe74994021b4d9546c3867b33e2
Marcos Sousa <msouzaantonio021@gmail.com>
2026-05-15 21:14:42 +0000

    calculadora de viagens

--- calculdora_viagem.py
+++ calculdora_viagem.py
@@ -3,14 +3,16 @@ destino = input( "digite o nome da cidade/paísde destino:  ")
 custo_passagem = float( input ("digite o custo da passagem em reais (BRL)"))
 custo_diario_euro = float(input("digite o custo diario da hospedagem em euro(EUR): "))
 quantidade_de_dias = int(input("digite a quantidade de dias da viagem: "))
-hospedagem_total_BRL = (custo_diario_euro*6.10)
-custo_total = custo_passagem + hospedagem_total_BRL
-print("-------------------------")
-print("custo total da hospedagem em BRL: R$", custo_total)
-if orçamento_total >= custo_total:
-    print("Sucesso! Seu orçamento é suficiente.")
+if orçamento_total<0 or custo_passagem<0 or custo_diario_euro <0 or quantidade_de_dias<0:
+    print("erro:não são aceitos valores negativos!")
 else:
-    faltante = custo_total - orçamento_total
-    print("orçamento insuficiente.")
-    print("faltam para completar a viagem : R$", faltante)
-          
+    hospedagem_total_BRL = (custo_diario_euro*6.10)
+    custo_total = custo_passagem + hospedagem_total_BRL
+    print("-------------------------")
+    print("custo total da hospedagem em BRL: R$", custo_total)
+    if orçamento_total >= custo_total:
+        print("Sucesso! Seu orçamento é suficiente.")
+    else:
+        faltante = custo_total - orçamento_total
+        print("orçamento insuficiente.")
+        print("faltam para completar a viagem : R$", faltante)
